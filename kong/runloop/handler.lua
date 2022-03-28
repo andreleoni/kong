3b8e9da370ab86f166bd7eb02a9d907d06d037e1
Alan Boudreault <alan@alanb.ca>
2022-03-28 15:42:44 -0400

    apply lmdb to 2.8.0 release

--- kong/runloop/handler.lua
+++ kong/runloop/handler.lua
@@ -9,7 +9,6 @@ local constants    = require "kong.constants"
 local singletons   = require "kong.singletons"
 local certificate  = require "kong.runloop.certificate"
 local concurrency  = require "kong.concurrency"
-local declarative  = require "kong.db.declarative"
 local workspaces   = require "kong.workspaces"
 local lrucache     = require "resty.lrucache"
 
@@ -362,15 +361,15 @@ local function register_events()
 
     worker_events.register(function(default_ws)
       if ngx.worker.exiting() then
-        log(NOTICE, "declarative flip config canceled: process exiting")
+        log(NOTICE, "declarative reconfigure canceled: process exiting")
         return true
       end
 
       local ok, err = concurrency.with_coroutine_mutex(FLIP_CONFIG_OPTS, function()
-        balancer.stop_healthcheckers(CLEAR_HEALTH_STATUS_DELAY)
+        kong.core_cache:purge()
+        kong.cache:purge()
 
-        kong.cache:flip()
-        core_cache:flip()
+        balancer.stop_healthcheckers(CLEAR_HEALTH_STATUS_DELAY)
 
         kong.default_workspace = default_ws
         ngx.ctx.workspace = kong.default_workspace
@@ -380,22 +379,18 @@ local function register_events()
 
         balancer.init()
 
-        declarative.lock()
-
         return true
       end)
 
       if not ok then
         log(ERR, "config flip failed: ", err)
       end
-    end, "declarative", "flip_config")
+    end, "declarative", "reconfigure")
 
     return
   end
 
-
   -- events dispatcher
-
 
   worker_events.register(function(data)
     if not data.schema then
